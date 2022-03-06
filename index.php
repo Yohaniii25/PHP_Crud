bc44e5998774a55bed318d41aee07564c5cb97e4
Yohaniii25 <yohanii725@gmail.com>
2022-03-07 01:33:36 +0530

    finished the insertion of data

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+include 'connect.php';
+if (isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobile'];
+    $password=$_POST['password'];
+
+    $sql="insert into `crud` (name,email,mobile,password)
+    values('$name','$email','$mobile','$password')";
+    $result = mysqli_query($con,$sql);
+    if($result){
+        echo "data inserted";
+    }else{
+        die(mysqli_error($con));
+    }
+}
+?>
+<!-- php -->
+
 <!doctype html>
 <html lang="en">
 
